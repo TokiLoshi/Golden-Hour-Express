559727535305e0e3804fdfaf1783f426eda224f9
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2026-04-15 19:28:59 -0700

    debugging the model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -100,27 +100,17 @@ function Train() {
 	// const { scene } = useGLTF(
 	// 	"/models/train/spirited_away_train_fanart/scene.gltf",
 	// );
-	// useLayoutEffect(() => {
-	// 	scene.traverse((obj) => {
-	// 		if (obj.isMesh) {
-	// 			obj.material.transparent = false;
-	// 			obj.material.side = THREE.DoubleSide;
-	// 			obj.material.opacity = 1;
-	// 		}
-	// 	});
-	// }, [scene]);
-
-	useMemo(() => {
-		scene.traverse((child) => {
-			if (child.mesh) {
-				child.material.side = THREE.DoubleSide;
-				child.material.transparent = false;
-				child.material.opacity = 1;
-				child.material.alphaTest = 0;
-				child.material.depthWrite = true;
+	useLayoutEffect(() => {
+		scene.traverse((obj) => {
+			if (obj.isMesh) {
+				obj.material.transparent = false;
+				obj.material.side = THREE.DoubleSide;
+				obj.material.opacity = 1;
+				obj.material.depthWrite = true;
 			}
 		});
 	}, [scene]);
+
 	const { posX, posY, posZ, rotX, rotY, rotZ, scale } = useControls(
 		"train",
 		{
