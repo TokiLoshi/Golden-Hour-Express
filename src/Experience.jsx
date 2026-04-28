397f5427e80d1f678140111920bfd8728a661588
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2026-04-28 15:30:32 -0700

    adding postprocessing

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,5 +1,5 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef, useMemo, useLayoutEffect } from "react";
+import { useRef, useMemo, useLayoutEffect, useState, useEffect } from "react";
 import vertexShader from "../src/shaders/nebula/vertex.glsl";
 import fragmentShader from "../src/shaders/nebula/fragment.glsl";
 import * as THREE from "three";
@@ -7,40 +7,16 @@ import { useControls } from "leva";
 import { useGLTF } from "@react-three/drei";
 import Nebula from "./Nebula";
 import Train from "./Train";
-
-function PlaceholderTrain() {
-	const trainRef = useRef();
-
-	useFrame(({ clock }) => {
-		const t = clock.getElapsedTime();
-
-		trainRef.current.rotation.z =
-			Math.sin(t * 1.8) * 0.018 + Math.sin(t * 3.1) * 0.008;
-		trainRef.current.rotation.x = Math.sin(t * 0.9) * 0.012;
-		trainRef.current.rotation.y =
-			Math.sin(t * 1.2) * 0.05 + Math.sin(t * 2.7) * 0.02;
-
-		// Replace with camera movement
-		trainRef.current.position.z = Math.sin(t * 0.3) * 0.08;
-	});
-
-	return (
-		<>
-			<mesh ref={trainRef} position={[0, 0, 0]}>
-				<boxGeometry args={[0.8, 0.5, 2.0]} />
-				<meshStandardMaterial
-					color='#f4a832'
-					emissive='#c4620a'
-					roughness={0.6}
-					metalness={0.3}
-				/>
-				<pointLight color='#fa832' intensity={3} distance={4} decay={2} />
-			</mesh>
-		</>
-	);
-}
+import { Bloom, EffectComposer, GodRays } from "@react-three/postprocessing";
 
 export default function Experience({ audioData, update }) {
+	const sunRef = useRef();
+	const [sunReady, setSunReady] = useState(false);
+	useEffect(() => {
+		if (sunRef.current) {
+			setSunReady(true);
+		}
+	}, []);
 	return (
 		<>
 			<Nebula audioData={audioData} update={update} />
@@ -61,6 +37,29 @@ export default function Experience({ audioData, update }) {
 				decay={2}
 				position={[0, 3, 0]}
 			/>
+			{sunReady && (
+				<EffectComposer multisampling={0} disableNormalPas>
+					<GodRays
+						sun={sunRef}
+						blendFunction={BlendFunction.Screen}
+						samples={60}
+						density={0.96}
+						decay={0.92}
+						weight={0.5}
+						exposure={0.55}
+						width={Resizer.AUTO_SIZE}
+						heihg={Resizer.AUTO_SIZE}
+						kernelSize={KernalSize.SMALL}
+						blur
+					/>
+					<Bloom
+						intensity={0.45}
+						luminanceThreshold={0.6}
+						luminanceSmoothing={0.2}
+						mipmapBlur
+					/>
+				</EffectComposer>
+			)}
 		</>
 	);
 }
